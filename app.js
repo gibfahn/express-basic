b834210d8119443467b656dcdd43d6f7d4d25d53
Beth Griggs <Bethany.Griggs@uk.ibm.com>
2017-02-09 11:59:41 +0000

    comment optional code,change appmetrics requires

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-// Addon: monitoring
-const appmetrics = require('appmetrics');
-const monitoring = appmetrics.monitor();
+// *** OPTION: Monitoring ***
+require('appmetrics-dash').start()
+
+// *** OPTION: CLOUDANT ***
+// Load the Cloudant library.
+// const Cloudant = require('cloudant');
+// const me = 'nodejs'; // Set this to your own account
+// const password = process.env.cloudant_password;
+// // Initialize the library with my account.
+// let cloudant = Cloudant({
+//   account: me,
+//   password: password
+// });
+//
+// cloudant.db.list(function(err, allDbs) {
+//   console.log('All my databases: %s', allDbs.join(', '))
+// });
 
 const express = require('express');
 const app = express();
 
-// Addon: web
+// *** APPTYPE: WEB ***
 /*
 app.use(express.static(__dirname + '/public'));
 */
 const port = 'PORT' in process.env ? process.env.PORT : 8080
 
-app.listen(port, function () {
+// TODO: VCAP_SERVICES
+//const services = JSON.parse(process.env.VCAP_SERVICES || "{}");
+
+app.listen(port, function() {
   console.log(`Example app listening on port ${this.address().port}!`)
 })
