603029fefaf52356873bccc6025c588abd963186
Gibson Fahnestock <gib@uk.ibm.com>
2017-02-10 18:52:36 +0800

    Add mongodb and uncomment app.js code
    I tried a variety of label/type combinations:
     - mongo Default
     - mongodb Default
     - mongodb 100
    
    But couldn't work out which was the right one

--- app.js
+++ app.js
@@ -1,77 +1,90 @@
-// *** OPTION: Monitoring ***
+/*** OPTION: Monitoring - START ***/
 require('appmetrics-dash').start()
+/*** OPTION: Monitoring - END ***/
 
 const express = require('express');
 const app = express();
 
-// *** APPTYPE: WEB ***
-/*
+/*** APPTYPE: WEB - START ***/
 app.use(express.static(__dirname + '/public'));
-*/
+/*** APPTYPE: WEB - END ***/
 
-// *** OPTION: CLOUDANT ***
+/*** OPTION: CLOUDANT - START ***/
 // Load the Cloudant library.
-// const Cloudant = require('cloudant');
-// const me = 'nodejs'; // Set this to your own account
-// const password = process.env.cloudant_password;
-// // Initialize the library with my account.
-// let cloudant = Cloudant({
-//   account: me,
-//   password: password
-// });
-//
-// cloudant.db.list(function(err, allDbs) {
-//   console.log('All my databases: %s', allDbs.join(', '))
-// });
+const Cloudant = require('cloudant');
+const me = 'nodejs'; // Set this to your own account
+const password = process.env.cloudant_password;
+// Initialize the library with my account.
+let cloudant = Cloudant({
+  account: me,
+  password: password
+});
 
-// *** OPTION: MONGODB ***
-// var MongoClient = require('mongodb').MongoClient,
-//   format = require('util').format;
-//
-// if (process.env['PORT']) {
-//  port = process.env['PORT'];
-// } else {
-//  port = '27017';
-// }
-//
-// hostname = process.env['MONGO_HOSTNAME'];
-// dbName = process.env['MONGO_DBNAME'];
-//
-// MongoClient.connect('mongodb://' + hostname + ':' + port + '/' + dbName,
-// function (err, db) {
-//     if (err) {
-//         throw err;
-//     } else {
-//         console.log("Successfully connected to the database");
-//     }
-//     db.close();
-// });
+cloudant.db.list(function(err, allDbs) {
+  if (allDbs) {
+    console.log('All my databases: %s', allDbs.join(', '))
+  } else {
+    console.log('No cloudant databases found');
+  }
+});
+/*** OPTION: CLOUDANT - END ***/
 
-// *** OPTION: REDIS
-// const redis = require('redis');
-//
-// if (process.env['PORT']) {
-//   port = process.env['PORT'];
-// } else {
-//   port = '6379';
-// }
-// if (process.env['HOSTNAME']) {
-//   host = process.env['HOSTNAME'];
-// } else {
-//   host = '127.0.0.1';
-// }
-//
-// var client = redis.createClient(port, host);
-// // if you'd like to select database 3, instead of 0 (default), call
-// // client.select(3, function() { /* ... */ });
-//
-// client.on('error', function (err) {
-//     console.log('Error ' + err);
-// });
-// client.on('connect', function() {
-//     console.log('Successfully connected to the database');
-// });
-// client.quit();
+/*** OPTION: MONGODB - START ***/
+// TODO(gib): Fix the manifest.yml so it provisions the service properly, then 
+// uncomment this section (which seems to work locally).
+/* 
+var MongoClient = require('mongodb').MongoClient,
+  format = require('util').format;
+
+let mongoPort;
+if (process.env['PORT']) {
+ mongoPort = process.env['PORT'];
+} else {
+ mongoPort = '27017';
+}
+
+hostname = process.env['MONGO_HOSTNAME'] ? process.env['MONGO_HOSTNAME'] : 'localhost';
+dbName = process.env['MONGO_DBNAME'];
+
+MongoClient.connect('mongodb://' + hostname + ':' + mongoPort + '/' + dbName,
+function (err, db) {
+    if (err) {
+        throw err;
+    } else {
+        console.log("Successfully connected to the mongodb database");
+    }
+    db.close();
+});
+*/
+/*** OPTION: MONGODB - END ***/
+
+/*** OPTION: REDIS - START ***/
+const redis = require('redis');
+let redisPort;
+
+if (process.env['PORT']) {
+  redisPort = process.env['PORT'];
+} else {
+  redisPort = '6379';
+}
+if (process.env['HOSTNAME']) {
+  host = process.env['HOSTNAME'];
+} else {
+  host = '127.0.0.1';
+}
+
+var client = redis.createClient(redisPort, host);
+// if you'd like to select database 3, instead of 0 (default), call
+// client.select(3, function() { /* ... */ });
+
+client.on('error', function (err) {
+    console.log('Error ' + err);
+});
+client.on('connect', function() {
+    console.log('Successfully connected to the database');
+});
+client.quit();
+/*** OPTION: REDIS - END ***/
 
 const port = 'PORT' in process.env ? process.env.PORT : 8080
 
@@ -79,5 +92,5 @@ const port = 'PORT' in process.env ? process.env.PORT : 8080
 //const services = JSON.parse(process.env.VCAP_SERVICES || "{}");
 
 app.listen(port, function() {
-  console.log(`Example app listening on ${this.address().address}:${this.address().port}!`)
+  console.log(`Example app listening on ${this.address().address}:${this.address().port}`)
 })
